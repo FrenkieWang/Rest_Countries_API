1fe5c672b8563c4b12fc7076d6f1034e7f53cefa
FrenkieWang <FrenkieWang21@gmail.com>
2024-01-01 13:22:51 +0000

    Modify comments

--- frontend/src/MapComponent.js
+++ frontend/src/MapComponent.js
@@ -30,9 +30,9 @@ function MyMapComponent (props) {
 
 export default MyMapComponent;
 
-// 以 REACT_APP_ 开头的环境变量名是必须的，因为 Create React App 只会将这些环境变量嵌入到客户端 JavaScript 中。
+// Env Varialbe in Frontend must starts with "REACT_APP_"
 // npm install --force --save @react-google-maps/api
 
 // fetch(`http://localhost:5000/api/resolve-map-url?url=${props.mapUrl}`)
 // Change [localhost:5000] into [deployed backend server]
-// Backend Server" rest-countries-api-backend.vercel.app
+// Backend Server: rest-countries-api-backend.vercel.app
