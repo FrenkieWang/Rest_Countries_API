6dd2789c670514270b32bc55b0d3875709ce3b77
FrenkieWang <FrenkieWang21@gmail.com>
2024-01-02 18:55:40 +0000

    Filter Country by A - Z

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -6,6 +6,7 @@ function HomePage(){
     const [data, setData] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
     const [selectedContinent, setSelectedContinent] = useState('');
+    const [selectedLetter, setSelectedLetter] = useState('');
   
     useEffect(() => {
       axios.get('https://restcountries.com/v3.1/all') 
@@ -15,6 +16,10 @@ function HomePage(){
         })
         .catch(error => console.error('Error fetching data:', error));
     }, []);
+
+    function handleLetterClick (letter) {
+      setSelectedLetter(letter);
+    }
 
     function sortCountryNameAsc(countryA, countryB) {
       let comparison = 0;
@@ -26,7 +31,7 @@ function HomePage(){
       return comparison;
     }
     
-    function countryFilterFunction(searchTerm) {
+    function FilterCountriesbyTerms(searchTerm) {
       return function (countryObject) {
         let commonName = countryObject.name.common.toLowerCase();
         let region = countryObject.region.toLowerCase();
@@ -36,13 +41,27 @@ function HomePage(){
         );
       };
     }
+    
+    function FilterCountriesByLetter(selectedLetter){
+      return function (countryObject) {
+        if(selectedLetter === ''){
+          return countryObject;
+        } 
+        else{
+          return countryObject.name.common
+          .normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+          .startsWith(selectedLetter);
+        }
+      }
+    }   
   
     return(
       <div>
         <h1>Bonuce Insights Challenge</h1>
         <h3> 
-          Search Keyword:  <span style = {{color:'blue'}}>&nbsp;&nbsp;{searchTerm}</span>
-          Selected Option:  <span style = {{color:'blue'}}>{selectedContinent}</span>
+          Search Keyword: <span style = {{color:'blue'}}>&nbsp;{searchTerm}&nbsp;</span>
+          Selected Option: <span style = {{color:'blue'}}>&nbsp;{selectedContinent}&nbsp;</span>
+          Selected Letter: <span style = {{color:'blue'}}>&nbsp;{selectedLetter}&nbsp;</span>
         </h3>
   
         <h3>Type your search keyword here: </h3>
@@ -58,9 +77,22 @@ function HomePage(){
           <option value="Antarctic">Antarctic</option>
           <option value="Oceania">Oceania</option>
         </select>      
+
+        <h3>Select by the Capital:</h3>  
+        <div>
+          {/* Show All Countries */}
+          <button onClick={() => handleLetterClick('')}>All</button>
+          {/* A - Z */}
+          {Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i)).map(letter => (
+            <button key={letter} onClick={() => handleLetterClick(letter)}>
+              {letter}
+            </button>
+          ))}
+        </div>
   
         <div style={styles.container}>
-          {data.filter(countryFilterFunction(searchTerm))
+          {data.filter(FilterCountriesbyTerms(searchTerm))
+            .filter(FilterCountriesByLetter(selectedLetter))
             .sort(sortCountryNameAsc)
             .map((item, index) => 
               <LinkCard key={index} data={item} />
